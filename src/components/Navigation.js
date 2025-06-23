8513233ebcafab82f0a55be276703a57a3361d70
black-lacksun <oiokami6@gmail.com>
2025-06-23 14:42:44 +0900

    Update Navigation.js

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -12,17 +12,17 @@ function Navigation() {
     <span>一覧</span>
   </Link>
       |
-      <Link to="/add" className="inline-flex items-center space-x-1">
+      <Link to="/add" className="inline-flex items-center space-x-1 hover:text-blue-600">
         <FcDataRecovery />
         <span>追加</span>
       </Link>
       |
-      <Link to="/delete" className="inline-flex items-center space-x-1">
+      <Link to="/delete" className="inline-flex items-center space-x-1 hover:text-blue-600">
         <FcFullTrash />
         <span>削除</span>
       </Link>
       |
-      <Link to="/find" className="inline-flex items-center space-x-1">
+      <Link to="/find" className="inline-flex items-center space-x-1 hover:text-blue-600">
         <FaSearchengin />
         <span>検索</span>
       </Link>
